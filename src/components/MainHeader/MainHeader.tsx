9354b275fba6ef3a83cb4e77d771cd0d9f8e3e68
skozenkov <skozenkov@deloitte.com>
2024-07-02 18:17:56 +0300

    Add logic

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,22 +1,26 @@
 import Link from "next/link";
+import {ServerIcon, UserCircleIcon} from "@heroicons/react/16/solid";
+import UkraineFlag from '../../shared/images/ukraine-flag-icon.svg'
 import Image from 'next/image'
-import mainIcon from  '../../shared/images/main.png'
 
 const HeaderPage = () => {
     return (
         <header className="p-4 bg-green">
-            <ul className={'flex gap-2 justify-center items-center'}>
+            <ul className={'flex gap-52 justify-center items-center'}>
                 <li>
                     <Link href={'/'}>
-                        <Image
-                            src={mainIcon as string}
-                            width={50}
-                            height={50}
-                            alt={'Main beautiful icon'}
-                        />
+                        <ServerIcon height={40} width={40} />
                     </Link></li>
-                <li><Link href={'/registration'}>Зареєструватись</Link></li>
-                <li><Link href={'/login'}>Увійти</Link></li>
+                <li>
+                    <Link href={'/login'} className={'flex gap-1'}>
+                        <UserCircleIcon height={20} width={20} />
+                        Зареєструватись/Увійти
+                    </Link>
+                </li>
+                <li className={'flex gap-1'}>
+                    <Image src={UkraineFlag as string} width={30} alt={'ukraine-flag'} />
+                    UA
+                </li>
             </ul>
         </header>
     )
